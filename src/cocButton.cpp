73f8d7b68988c3bd31c88000a8a1845b68015474
lukasz karluk <julapy@googlemail.com>
2016-05-29 23:37:42 +1000

    cocButton is done but still needs some testing.

--- src/cocButton.cpp
+++ src/cocButton.cpp
@@ -8,12 +8,162 @@
 
 namespace coc {
 
-Button::Button() {
-    //
+Button::Button(coc::Rect rect) {
+
+    setRect(rect);
+    bUseHandlers = false;
+
+    bOver = coc::Value<bool>(false); // init with default value.
+    bDown = coc::Value<bool>(false); // init with default value.
 }
 
 Button::~Button() {
     //
+}
+
+//--------------------------------------------------------------
+void Button::setRect(coc::Rect value) {
+    rect = value;
+}
+
+void Button::setRect(float x, float y, float w, float h) {
+    coc::Rect r;
+    r.setX(x);
+    r.setY(y);
+    r.setW(w);
+    r.setH(h);
+    setRect(r);
+}
+
+//--------------------------------------------------------------
+void Button::update() {
+    
+    bOver.update();
+    bDown.update();
+    
+    if(bUseHandlers == false) {
+        return;
+    }
+    
+    if(movedInside()) {
+        handleMovedInside();
+    }
+    if(movedOutside()) {
+        handleMovedOutside();
+    }
+    if(pressedInside()) {
+        handlePressedInside();
+    }
+    if(draggedInside()) {
+        handleDraggedInside();
+    }
+    if(draggedOutside()) {
+        handleDraggedOutside();
+    }
+    if(releasedInside()) {
+        handleReleasedInside();
+    }
+    if(releasedOutside()) {
+        handleReleasedOutside();
+    }
+}
+
+bool Button::over() const {
+    return bOver;
+}
+
+bool Button::overChanged() const {
+    return bOver.hasChanged();
+}
+
+bool Button::down() const {
+    return bDown;
+}
+
+bool Button::downChanged() const {
+    return bDown.hasChanged();
+}
+
+//--------------------------------------------------------------
+bool Button::movedInside() {
+    return (over() && overChanged());
+}
+
+bool Button::movedOutside() {
+    return (!over() && overChanged());
+}
+
+bool Button::pressedInside() {
+    return (down() && downChanged());
+}
+
+bool Button::draggedInside() {
+    return (!down() && over() && overChanged());
+}
+
+bool Button::draggedOutside() {
+    return (!down() && downChanged() && !over() && overChanged());
+}
+
+bool Button::releasedInside() {
+    return (!down() && downChanged() && over());
+}
+
+bool Button::releasedOutside() {
+    return (!down() && downChanged() && !over());
+}
+
+//--------------------------------------------------------------
+void Button::pointMoved(int x, int y) {
+    bOver = rect.isInside(x, y);
+}
+
+void Button::pointPressed(int x, int y) {
+    bOver = rect.isInside(x, y);
+    if(bOver == true) {
+        bDown = true;
+    }
+}
+
+void Button::pointDragged(int x, int y) {
+    bOver = rect.isInside(x, y);
+    if(bOver == false) {
+        bDown = false;
+    }
+}
+
+void Button::pointReleased(int x, int y) {
+    bOver = rect.isInside(x, y);
+    bDown = false;
+}
+
+//--------------------------------------------------------------
+void Button::handleMovedInside() {
+    // override.
+}
+
+void Button::handleMovedOutside() {
+    // override.
+}
+
+void Button::handlePressedInside() {
+    // override.
+}
+
+void Button::handleDraggedInside() {
+    // override.
+}
+
+void Button::handleDraggedOutside() {
+    // override.
+}
+
+void Button::handleReleasedInside() {
+    // override.
+}
+
+void Button::handleReleasedOutside() {
+    // override.
 }
 
 }
--- src/cocButton.h
+++ src/cocButton.h
@@ -10,13 +10,52 @@
 
 namespace coc {
 
+//--------------------------------------------------------------
 class Button {
 
 public:
 
-    Button();
+    Button(coc::Rect rect = coc::Rect());
     ~Button();
+    
+    void setRect(coc::Rect rect);
+    void setRect(float x, float y, float w, float h);
+    
+    void update();
+    
+    bool over() const;
+    bool overChanged() const;
+    bool down() const;
+    bool downChanged() const;
+    
+    bool movedInside();
+    bool movedOutside();
+    bool pressedInside();
+    bool draggedInside();
+    bool draggedOutside();
+    bool releasedInside();
+    bool releasedOutside();
 
+	void pointMoved(int x, int y);
+    void pointPressed(int x, int y);
+	void pointDragged(int x, int y);
+	void pointReleased(int x, int y);
+    
+    virtual void handleMovedInside();
+    virtual void handleMovedOutside();
+    virtual void handlePressedInside();
+    virtual void handleDraggedInside();
+    virtual void handleDraggedOutside();
+    virtual void handleReleasedInside();
+    virtual void handleReleasedOutside();
+    
+protected:
+
+    coc::Rect rect;
+    bool bUseHandlers;
+    
+    coc::Value<bool> bOver;
+    coc::Value<bool> bDown;
 };
 
 }
